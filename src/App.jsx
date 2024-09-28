72ef6aab4d0176aac0fcd1abe2b203449768e88d
Suraj Nair <surajnair9698@gmail.com>
2024-09-28 18:09:45 +0530

    Added the logic of adding todos to the localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoInput from "./Components/TodoInput";
 import TodoList from "./Components/TodoList";
 
@@ -6,9 +6,18 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   const [todoValue, setTodoValue] = useState("");
+  function persistData(newList) {
+    localStorage.setItem(
+      "todos",
+      JSON.stringify({
+        todos: newList,
+      })
+    );
+  }
 
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo];
+    persistData(newTodoList);
     setTodos(newTodoList);
   }
 
@@ -16,6 +25,7 @@ function App() {
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex !== index;
     });
+    persistData(newTodoList);
     setTodos(newTodoList);
   }
 
@@ -24,6 +34,17 @@ function App() {
     setTodoValue(valueToBeEdited);
     handleDeleteTodo(index);
   }
+  useEffect(() => {
+    if (!localStorage) {
+      return;
+    }
+    let localTodos = localStorage.getItem("todos");
+    if (!localTodos) {
+      return;
+    }
+    localTodos = JSON.parse(localTodos).todos;
+    setTodos(localTodos);
+  }, []);
   return (
     <>
       <TodoInput
